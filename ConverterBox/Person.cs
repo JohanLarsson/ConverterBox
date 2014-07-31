4b40939abb01ef7b3c1a623ad342b302d6b7ec27
Johan Larsson <johan_gavle1@hotmail.com>
2014-07-31 20:28:50 +0200

    Added listboxes bound to the same source

---
+++ ConverterBox/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace ConverterBox
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using ConverterBox.Annotations;
+
+    public class Person : INotifyPropertyChanged
+    {
+        private string name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value == this.name)
+                {
+                    return;
+                }
+                this.name = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public int Age { get; set; }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- ConverterBox/Vm.cs
+++ ConverterBox/Vm.cs
@@ -1,13 +1,24 @@
 ﻿namespace ConverterBox
 {
+    using System.Collections.ObjectModel;
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
     using Annotations;
 
     public class Vm : INotifyPropertyChanged
     {
+        private readonly ObservableCollection<Person> _persons = new ObservableCollection<Person>();
         private string _stringValue;
         private double _doubleValue;
+
+        public Vm()
+        {
+            _persons.Add(new Person { Name = "Reed", Age = 38 });
+            _persons.Add(new Person { Name = "Johan", Age = 36 });
+            _persons.Add(new Person { Name = "Brad", Age = 38 });
+            _persons.Add(new Person { Name = "George", Age = 41 });
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public string StringValue
@@ -38,6 +49,13 @@
             }
         }
 
+        public ObservableCollection<Person> Persons
+        {
+            get
+            {
+                return _persons;
+            }
+        }
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
